3600f32045ea46f54e6764c05d92a9c5b2d0bf66
midhunsushil <32041229+midhunsushil@users.noreply.github.com>
2017-12-24 13:22:04 +0300

    Add files via upload

---
+++ main/design.h
@@ -0,0 +1,26 @@
+#include <fstream.h>
+#include <conio.h>
+ void delay(unsigned short p, unsigned short q = 4) {
+  for (unsigned long i = 0; i < (p * 10); i++)
+	for (unsigned long j = 0; j < (q * 1000); j++) { /*To delay output*/ }
+ }
+void welcome() {
+ ifstream design, font;
+ design.open("design3.txt", ios:: in );
+ char ch;
+ while (design.get(ch)) {
+  cout << ch;
+  if (ch == '\n') delay(400, 8);
+ }
+ design.close();
+ getch();
+ //gotoxy(1,1);
+ cout << "\n  WELCOME TO....\n";
+ font.open("font.txt", ios:: in );
+ while (font.get(ch)) {
+  cout << ch;
+  delay(20,2);
+ }
+ font.close();
+ getch();
+}
